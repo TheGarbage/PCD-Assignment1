c9792db991a0c27180f95211342b988cdd96e08c
DavideBucchieri <davide.bucchieri@studio.unibo.it>
2023-04-05 23:15:06 +0200

    v1.7.0.1 ui non responsive

--- src/MainView.java
+++ src/MainView.java
@@ -3,6 +3,6 @@ import Thread.ThreadMaster;
 public class MainView {
 
     public static void main(String[] args) throws InterruptedException {
-        ThreadMaster threadMaster = new ThreadMaster();
+        new ThreadMaster();
     }
 }
